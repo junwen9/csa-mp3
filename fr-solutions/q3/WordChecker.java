3a3f7c4bf072451ab31f2ab8e28a37f747c7e6e2
junwen9 <147454038+junwen9@users.noreply.github.com>
2024-05-22 11:20:17 -0400

    Create WordChecker.java

---
+++ fr-solutions/q3/WordChecker.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class WordChecker {
+  private ArrayList<String> wordList;
+
+  public WordChecker(ArrayList<String> words) {
+    wordList = words;
+  }
+
+  public boolean isWordChain() {
+    for(int i = 1; i<wordList.size();i++){
+      if(!(wordList.get(i).indexOf(wordList.get(i-1))!=-1)){
+         return false;
+      }
+    }
+    return true;
+  }
+
+
+  public ArrayList<String> createList(String target) {
+    ArrayList<String> result = new ArrayList<String>();
+    for(int i= 0; i<wordList.size();i++){
+      if(wordList.get(i).indexOf(target)==0){
+        
+        result.add(wordList.get(i).substring(target.length()));
+      }
+      
+    }
+    
+    return result;             
+  }
+}
